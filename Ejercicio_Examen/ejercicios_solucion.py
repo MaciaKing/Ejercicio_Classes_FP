a41b4dd1a573bec1edb13dfe00636da7089f3d88
MaciaKing <maciasalvasalva@gmail.com>
2023-12-15 18:30:50 +0100

    solucion

--- Ejercicio_Examen/ejercicios_solucion.py
+++ Ejercicio_Examen/ejercicios_solucion.py
@@ -7,12 +7,12 @@ class Coche():
         self.caballos=caballos
 
     def to_string(self):
-        return "{marca:", self.nombre, "caballos: ", self.caballos, "}"
+        return "{marca:"+ self.nombre+ ", caballos: "+ self.caballos+ "}"
     
 class Carrera():
     def __init__(self, coches):
         self.coches=coches
-        self.resultado=[] #### UTILIZAR
+        self.resultado=[]
 
     def mostrar_parrilla_de_salida(self):
         aux = 1
@@ -21,16 +21,16 @@ class Carrera():
             aux = aux+1
     
     def empieza_carrera(self):
-        print("La carrera ha empezado..")
-
-    def finaliza_carrera(self):
-        # Generar randoms entre mis self.coches
-        # shuffle
-        self.resultado.append(self.coches[ran])
-        None
+        print("La carrera empieza... ")
     
+    def finaliza_carrera(self):
+        aux = self.coches
+        while not len(aux) == 0:
+            ran = random.randint(0, len(aux)-1)
+            self.resultado.append(aux[ran])
+            del aux[ran]
+
     def muestra_resultado(self):
-        # RECORRER (FOR SOBRE RESULTADO)
         for coche in self.resultado:
             print(coche.to_string())
 
@@ -38,7 +38,8 @@ if __name__ == '__main__':
     c1 = Coche("Mercedes", "120hp")
     c2 = Coche("Ferrari", "200hp")
     c3 = Coche("Mustang", "200hp")
-    coches=[c1, c2, c3]
+    c4 = Coche("mmm", "200hp")
+    coches=[c1, c2, c3, c4]
 
     c = Carrera(coches)
     c.mostrar_parrilla_de_salida()

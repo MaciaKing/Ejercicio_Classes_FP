dabfbc1893f330bc5e515ec694aa2a9387d205f1
MaciaKing <maciasalvasalva@gmail.com>
2023-12-15 15:52:27 +0100

    enunciado

--- Ejercicio_Examen/ejercicios.py
+++ Ejercicio_Examen/ejercicios.py
@@ -1,12 +1,34 @@
+import random
+import pdb
 
+class Coche():
+    def __init__(self, nombre, caballos):
+        self.nombre=nombre
+        self.caballos=caballos
 
+    def to_string(self):
+        return "{marca:", self.nombre, "caballos: ", self.caballos, "}"
+    
+class Carrera():
+    def __init__(self, coches):
+        self.coches=coches
+        self.resultado=[]
+
+    def mostrar_parrilla_de_salida(self):
+        aux = 1
+        for coche in self.coches:
+            print("El coche ", coche.nombre, "sale en [",aux,"] posicion")
+            aux = aux+1
+    
 
 if __name__ == '__main__':
     c1 = Coche("Mercedes", "120hp")
-    c2 = Coche("ferrari", "200hp")
-    c3 = Coche("adsfadsf", "asdf")
+    c2 = Coche("Ferrari", "200hp")
+    c3 = Coche("Mustang", "200hp")
+    coches=[c1, c2, c3]
 
-    coches= [c1, c2, c3]
     c = Carrera(coches)
+    c.mostrar_parrilla_de_salida()
     c.empieza_carrera()
+    c.finaliza_carrera()
     c.muestra_resultado()
